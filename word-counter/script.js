b2b467b3e0d38a7a9c57ec9a687dff4e297347b6
Maciej Browarski <140039573+coder36459@users.noreply.github.com>
2025-11-03 18:07:42 +0100

    Add files via upload

---
+++ word-counter/script.js
@@ -0,0 +1,7 @@
+function countWords(sentence) {
+    const numberOfWords = sentence.split(" ").length
+    sentence = numberOfWords
+
+    return sentence;
+}
+console.log(countWords("Hello world"))
